df97c26635dd46e552bf24ff3d69bb6c9735cf70
Igor Tuag <igortuag@gmail.com>
2020-07-01 07:29:29 -0300

    Migration e model de agendamento

---
+++ server/src/database/migrations/20200701101906-create-appointments.js
@@ -0,0 +1,45 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('appointments', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true
+      },
+      canceled_at: {
+        type: Sequelize.DATE
+      },
+      provider_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async queryInterface => {
+    return queryInterface.dropTable('appointments');
+  }
+};
