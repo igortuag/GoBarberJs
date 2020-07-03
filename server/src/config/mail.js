402bf20b77914da0d5708621cfb40ad6754b7820
Igor Tuag <igortuag@gmail.com>
2020-07-03 08:17:58 -0300

    Configurando Nodemailer

---
+++ server/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'bc9ee45dbb3cf6',
+    pass: 'dd5f88676bcf57'
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>'
+  }
+};
