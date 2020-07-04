4e9be50375aad384db94b134fa3bc8257a814156
Igor Tuag <igortuag@gmail.com>
2020-07-04 16:11:13 -0300

    Variaveis ambiente

--- server/src/config/mail.js
+++ server/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'a05804c345b95e',
-    pass: '20a9e0d301c5a1'
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>'
