5bdd427033bf11e31bc6ac9168c2f1b30187308f
Igor Tuag <igortuag@gmail.com>
2020-07-11 10:39:13 -0300

    Estilizacao da autenticacao

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
-export default class SignIn extends Component {
-  render() {
-    return <h1>Login</h1>;
-  }
+export default function SignIn() {
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
+  );
 }
